18d551aa30d0b4b7fadcb9a5ab30f6331b7045c8
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-20 01:35:59 +0100

    ajout de la classe test de adress ip

--- src/test/java/fr/uvsq/cprog/collex/DnsTest/AdressIPTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest/AdressIPTest.java
@@ -1,20 +1,33 @@
-package fr.uvsq.cprog.collex;
-
-import static org.junit.Assert.assertTrue;
-
+package fr.uvsq.cprog.collex.DnsTest;
+import static org.junit.Assert.assertEquals;
+import fr.uvsq.cprog.collex.Dns.AdresseIP;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * simple test unitaire pour la  classe addres ip
  */
-public class AppTest 
+public class AdressIPTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    /** test du constructeur */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test_constructeur (){
+        AdresseIP ip1 = new AdresseIP(192, 168 ,0 ,1);
+        assertEquals(192,ip1.getAdr());
+        assertEquals(168,ip1.getEs());
+        assertEquals(0,ip1.getSe());
+        assertEquals(1,ip1.getIp());
+
+
     }
+
+    /** test du toString */
+    @Test
+    public   void test_ToString (){
+        AdresseIP ip1 = new AdresseIP(192, 168 ,0 ,1);
+        assertEquals("192.168.0.1",ip1.toString());
+
+    }
+
+
 }
