ee9d75b28c85d63579b0a2a5e7c19ebd56e1aa63
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-21 21:42:36 +0100

    1.1/ ajout de de test de compare to et equals et hascode de la classe NomMachine

--- src/test/java/fr/uvsq/cprog/collex/DnsTest/NomMachineTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest/NomMachineTest.java
@@ -1,11 +1,13 @@
 package fr.uvsq.cprog.collex.DnsTest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import fr.uvsq.cprog.collex.Dns.NomMachine;
 import org.junit.Test;
-
+/**
+ *  fichier test de la classe NomMachine
+ *
+ * */
 public class NomMachineTest {
 
     @Test
@@ -18,10 +20,37 @@ public class NomMachineTest {
 
     @Test
     public  void test_ToString () {
-        NomMachine machine1 = new NomMachine("dell", "domaine", "mehdipc");
-        assertEquals("dell.domaine.mehdipc", machine1.toString());
+        NomMachine machine1 = new NomMachine("dell", "domaine", "local");
+        assertEquals("dell.domaine.local", machine1.toString());
 
     }
-  
+    @Test
+    public  void test_equals () {
+        NomMachine machine1  = new NomMachine("dell","domaine","local1");
+        NomMachine machine2  = new NomMachine("dell","domaine","locaL1");
+
+        assertEquals(true, machine1.equals(machine2));
+    }
+
+    @Test
+    public void test_compare_to (){
+        NomMachine machine1  = new NomMachine("hp","domaine1","local1");
+        NomMachine machine2  = new NomMachine("dell","domaine2","local1");
+        NomMachine machine3  = new NomMachine("hp","domaine1","local1");
+        assertEquals(1,machine1.compareTo(machine2));
+        assertEquals(-1,machine2.compareTo(machine1));
+
+        assertEquals(0,machine1.compareTo(machine3));
+
+    }
+
+    @Test
+    public void test_hascode (){
+        NomMachine machine1  = new NomMachine("dell","domaine","local1");
+        NomMachine machine2  = new NomMachine("dell","domaine","locaL1");
+
+        assertEquals(machine2.hashCode(),machine1.hashCode());
+
+    }
 
 }
