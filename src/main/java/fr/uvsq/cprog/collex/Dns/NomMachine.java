d0a3c73220b4449615d58f155673264ee2916f7b
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-21 19:30:41 +0100

    1.2 / ajout de la classe NomMachine avec son consturcteur et la methode to String

--- src/main/java/fr/uvsq/cprog/collex/Dns/NomMachine.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns/NomMachine.java
@@ -1,7 +1,48 @@
 package fr.uvsq.cprog.collex.Dns;
-
-import java.util.Objects;
+/** @author : debbah Mehdi sofiane
+ * classe qui represente un nom de machine
+ * */
 
 public class NomMachine {
+
+    private String machine;
+    private String domaine;
+    private String local;
+
+    public NomMachine(String machine, String domaine, String local) {
+        this.machine = machine;
+        this.domaine = domaine;
+        this.local = local;
+    }
+
+    public String getMachine() {
+        return machine;
+    }
+
+    public void setMachine(String machine) {
+        this.machine = machine;
+    }
+
+    public String getDomaine() {
+        return domaine;
+    }
+
+    public void setDomaine(String domaine) {
+        this.domaine = domaine;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    @Override
+    public String toString() {
+        return machine + "." + domaine + "." + local  ;
+    }
+
 
 }
