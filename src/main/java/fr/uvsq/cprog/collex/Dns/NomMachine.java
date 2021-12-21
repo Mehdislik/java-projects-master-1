439886553b9bfcb36921cf37a5913aa042439494
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-21 21:13:06 +0100

    1.1/ ajout de la methode equals , compare to , hascode de la classe NomMachine

--- src/main/java/fr/uvsq/cprog/collex/Dns/NomMachine.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns/NomMachine.java
@@ -1,9 +1,12 @@
 package fr.uvsq.cprog.collex.Dns;
+
+import java.util.Objects;
+
 /** @author : debbah Mehdi sofiane
  * classe qui represente un nom de machine
  * */
 
-public class NomMachine {
+public class NomMachine implements java.lang.Comparable<NomMachine>{
 
     private String machine;
     private String domaine;
@@ -45,4 +48,45 @@ public class NomMachine {
     }
 
 
+    @Override
+    public boolean equals(Object autre) {
+        if (this == autre) return true;
+        if (autre == null || getClass() != autre.getClass()) return false;
+        NomMachine that = (NomMachine) autre;
+        // deux nom de machine sont egaux meme si il sont en majuscule ou miniscule
+        String machinelower=this.machine.toLowerCase();
+        String domainelower=this.domaine.toLowerCase();
+        String locallower = this.local.toLowerCase();
+        String machineOtherlower=that.machine.toLowerCase();
+        String domaineOtherlower=that.domaine.toLowerCase();
+        String localOtherlower = that.local.toLowerCase();
+        return  (machinelower.equals(machineOtherlower) && domainelower.equals(domaineOtherlower) && locallower.equals(localOtherlower));}
+
+
+    @Override
+    public int hashCode() {
+        // mettre les nom de machine en miniscule
+        // pour avoir avoir le meme hascode si l'un est en majuscule et l'autre en minscule
+        String machinelower=this.machine.toLowerCase();
+        String domainelower=this.domaine.toLowerCase();
+        String locallower = this.local.toLowerCase();
+        return Objects.hash(machinelower, domainelower,locallower);
+    }
+
+    @Override
+    public int compareTo(NomMachine    autre) {
+
+        String nm1= this.machine+"."+this.domaine+"."+this.local;
+        String nm2= autre.machine+"."+autre.domaine+"."+autre.local;
+
+        int compare= nm1.compareTo(nm2);
+
+        if (compare >0){
+            return 1;
+        }else if (compare<0){
+            return  -1 ;
+        }
+        return 0;
+
+    }
 }
