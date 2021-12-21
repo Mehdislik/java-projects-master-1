23378f19a91ae0a58779bc9616abffaf144970cb
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-21 15:43:01 +0100

    ajout de methode equals et compare to et hascode de addresip

--- src/main/java/fr/uvsq/cprog/collex/Dns/AdresseIP.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns/AdresseIP.java
@@ -1,11 +1,16 @@
 package fr.uvsq.cprog.collex.Dns;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 /**
  *  par  debbah Mehdi sofaine */
+
+import java.util.Objects;
 
 /** definition de la classe d'adresse ip avec 4 champs :
  * adr.es.se.ip
  */
-public class AdresseIP {
+public class AdresseIP implements java.lang.Comparable<AdresseIP>  {
     private int adr;
     private int  es ;
     private int  se  ;
@@ -58,5 +63,44 @@ public class AdresseIP {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AdresseIP adresseIP = (AdresseIP) o;
+        return adr == adresseIP.adr && es == adresseIP.es && se == adresseIP.se && ip == adresseIP.ip;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(adr, es, se, ip);
+    }
+
+    @Override
+    public int compareTo(AdresseIP o) {
+
+        int i=0;
+        // creation de deux liste contenat chaqun une addre ip
+        ArrayList<Integer> IPother = new  ArrayList<Integer>();
+        IPother.add(o.adr);
+        IPother.add (o.es);
+        IPother.add (o.se);
+        IPother.add (o.ip);
+        ArrayList<Integer> IPothis = new  ArrayList<Integer>();
+        IPothis.add(this.adr);
+        IPothis.add (this.es);
+        IPothis.add (this.se);
+        IPothis.add (this.ip);
+
+        while ( i< 4){
+            if(IPothis.get(i).equals(IPother.get(i)))
+                i++;
+            else if( IPothis.get(i) < IPother.get(i))
+              return -1;
+            else if ( IPothis.get(i) > IPother.get(i))
+                return  1;
+
+        }
+        return 0;
+    }
 }
