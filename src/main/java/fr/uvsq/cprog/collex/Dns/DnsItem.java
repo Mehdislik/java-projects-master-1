f5be2d18715d948c936ad1f9418528c3d73844cc
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-21 22:04:00 +0100

    1.3/ajout de la classe DnsItem  avec son constructeur et la methode Tostring

--- src/main/java/fr/uvsq/cprog/collex/Dns/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns/DnsItem.java
@@ -1,4 +1,42 @@
 package fr.uvsq.cprog.collex.Dns;
 
 public class DnsItem {
+    /**
+     * @autor :debbah mehdi sofiane
+     * classe representant une entré DNS  :
+     * nomMachine addresIP
+     * tel que :
+     * nomMachine :nom.domaine.local
+     * **/
+
+
+    NomMachine nomMachine ;
+    AdresseIP adresseIP;
+
+    public DnsItem(NomMachine nom, AdresseIP adresseIP) {
+        this.nomMachine = nom;
+        this.adresseIP = adresseIP;
+    }
+
+    public NomMachine getNomMachine() {
+        return nomMachine;
+    }
+
+    public void setNomMachine(NomMachine nom) {
+        this.nomMachine = nom;
+    }
+
+    public AdresseIP getAdresseIP() {
+        return adresseIP;
+    }
+
+    public void setAdresseIP(AdresseIP adresseIP) {
+        this.adresseIP = adresseIP;
+    }
+
+    @Override
+    public String toString() {
+        return this.nomMachine.toString() +" "+ this.adresseIP.toString();
+
+    }
 }
