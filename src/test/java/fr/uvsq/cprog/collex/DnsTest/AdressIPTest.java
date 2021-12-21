c2a290926520486c19a2bcd02d7795d4e256517e
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-21 15:50:57 +0100

    ajout de des methode  test de equals et compare to et hascode

--- src/test/java/fr/uvsq/cprog/collex/DnsTest/AdressIPTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest/AdressIPTest.java
@@ -28,6 +28,45 @@ public class AdressIPTest
         assertEquals("192.168.0.1",ip1.toString());
 
     }
+    /** test de equals de deux  */
+    @Test
+    public void equals_test(){
+        AdresseIP ip1 = new AdresseIP(192, 168 ,0 ,2);
+        AdresseIP ip2 = new AdresseIP(192, 168 ,0 ,2);
+        assertEquals(true ,ip1.equals(ip2));
+    }
+    /** test de la fonction compare to  **/
+    @Test
+    public void test_compare_to() {
+
+        AdresseIP ip1 = new AdresseIP(192, 168 ,0 ,2);
+        AdresseIP ip2 = new AdresseIP(190, 168 ,0 ,2);
+        AdresseIP ip3 = new AdresseIP(190, 168,0,1);
+        AdresseIP ip4 = new AdresseIP(190, 168,0,1);
+
+        // test compare to si l'objet instancie   > l'objet en entrée
+        assertEquals(1 ,ip1.compareTo(ip2));
+        assertEquals(1 ,ip2.compareTo(ip3));
+
+        // test compare to si l'objet instancie   < l'objet en entrée
+        assertEquals(-1 ,ip2.compareTo(ip1));
+        assertEquals(-1 ,ip3.compareTo(ip2));
+
+        // test compare to si l'objet instancie   =l'objet en entrée
+        assertEquals(0 ,ip3.compareTo(ip4));
+
+    }
+
+    /** test du hascode   **/
+    @Test
+    public void test_hascode (){
+        AdresseIP ip1 = new AdresseIP(192, 168 ,0 ,2);
+        AdresseIP ip2 = new AdresseIP(192, 168 ,0 ,2);
+
+        assertEquals(ip2.hashCode(), ip1.hashCode() );
+
+    }
+
 
 
 }
