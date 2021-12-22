8c36c46ded1af6dc8dbc4344cf275847f4bb15a8
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-23 00:30:38 +0100

    package dessin : ajout de la classe test de la classe Point

---
+++ src/test/java/fr/uvsq/cprog/collex/DessinTest/PointTest.java
@@ -0,0 +1,28 @@
+package fr.uvsq.cprog.collex.DessinTest;
+import fr.uvsq.cprog.collex.dessin.Point;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * simple test unitaire pour la  classe Point
+ */
+public class PointTest {
+
+    @Test
+    public void test_constructeur (){
+        Point p = new Point(2,10);
+        assertEquals(2,p.getX(),0);
+        assertEquals(10,p.getY(),0);
+
+    }
+
+    @Test
+    public void  test_deplacer (){
+        Point p = new Point(2,10);
+        p.deplacer(4,5);
+
+        assertEquals(6,p.getX(),0);
+        assertEquals(15,p.getY(),0);
+    }
+
+}
