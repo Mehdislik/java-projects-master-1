3c3ec377097b33359f283b8d3fccf63cb95d9e3e
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-22 23:18:31 +0100

    package dessin : ajout de la classe Triangle

---
+++ src/main/java/fr/uvsq/cprog/collex/dessin/Triangle.java
@@ -0,0 +1,57 @@
+package fr.uvsq.cprog.collex.dessin;
+
+/**
+ * @author : debbah Mehdi Sofiane
+ *  classe represetant un triangle
+ *
+ **/
+public class Triangle extends Forme {
+    // variables
+    private Point a;
+    private Point b;
+    private Point c;
+
+    // Constructeur
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    // les methodes
+    public double coteA() {
+        return Math.sqrt(Math.pow(this.b.getX() - this.c.getX(), 2) + Math.pow(this.b.getY() - this.c.getY(), 2));
+    }
+
+    public double coteB() {
+        return  Math.sqrt(Math.pow(this.a.getX() - this.c.getX(), 2) + Math.pow(this.a.getY() - this.c.getY(), 2));
+    }
+
+    public double coteC() {
+        return Math.sqrt(Math.pow(this.a.getX() - this.b.getX(), 2) + Math.pow(this.a.getY() - this.b.getY(), 2));
+    }
+
+    public double Perimeter() {
+        return coteA() + coteB() + coteC();
+    }
+
+    public double Aire() {
+        double semiPerimeter = Perimeter() / 2;
+        return  Math.sqrt(semiPerimeter * ((semiPerimeter - coteA()) * (semiPerimeter - coteB()) * (semiPerimeter - coteC())));
+    }
+
+    public void deplacer(double dx, double dy) {
+        this.a.deplacer(dx, dy);
+        this.b.deplacer(dx, dy);
+        this.c.deplacer(dx, dy);
+    }
+
+    public void afficher() {
+        System.out.println("\nTriangle");
+        System.out.format("le point A est   (%.2f, %.2f)\n", this.a.getX(), this.a.getY());
+        System.out.format("le point B est  (%.2f, %.2f)\n", this.b.getX(), this.b.getY());
+        System.out.format("le Point c  (%.2f, %.2f)\n", this.c.getX(), this.c.getY());
+        System.out.format("Perimeter = %.2f cm\n", Perimeter());
+        System.out.format("Aire = %.2f cm²\n", Aire());
+    }
+}
