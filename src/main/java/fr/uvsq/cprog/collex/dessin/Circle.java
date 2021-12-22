e4a21d3ad72d6f5ab791b953bc7307bef404032a
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-22 23:16:35 +0100

    package dessin : ajout de la classe circle

---
+++ src/main/java/fr/uvsq/cprog/collex/dessin/Circle.java
@@ -0,0 +1,44 @@
+package fr.uvsq.cprog.collex.dessin;
+/**
+ * @author : debbah Mehdi Sofiane
+ *  classe represetant un circle
+ *
+ **/
+    public class Circle extends Forme {
+        // Attributes
+        private Point center;
+        private double radius;
+
+        // Constructor
+        public Circle(Point center, double radius) {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        // Methods
+        public double Diametere() {
+            return this.radius * 2;
+        }
+
+        public double Perimeter() {
+
+            return Math.PI * 2 * this.radius;
+        }
+
+        public double Aire() {
+            return Math.PI * Math.pow(this.radius, 2);
+        }
+
+        public void deplacer(double dx, double dy) {
+            this.center.deplacer(dx, dy);
+        }
+
+        public void afficher() {
+            System.out.println("\nCircle");
+            System.out.format("le centre du circle est a (%.2f, %.2f)\n", this.center.getX(), this.center.getY());
+            System.out.format("Diametere = %.2f cm\n", Diametere());
+            System.out.format("Perimetere = %.2f cm\n", Perimeter());
+            System.out.format("Aire = %.2f cm²\n", Aire());
+        }
+    }
+
