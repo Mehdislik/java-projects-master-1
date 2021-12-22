080f07687ac2f1a29c077688f75e40aedb213350
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-22 05:03:25 +0100

    5/ ajout du main de la classe DNsapp pour excuter les commande saisie

--- src/main/java/fr/uvsq/cprog/collex/Dns/DnsApp.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns/DnsApp.java
@@ -3,6 +3,11 @@ package fr.uvsq.cprog.collex.Dns;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * @author :debbah Mehdi sofiane
+ *
+ * classe qui va lire les commande de l'utlisateur  et les excuter
+ */
 public class DnsApp
 {
     private DnsTUI ui ;
@@ -23,5 +28,25 @@ public class DnsApp
 
 
 }
-    
+    public static void main( String[] args )throws  IOException
+    {
+
+        System.out.println("bienvnue dans le serveur DNS");
+        DnsApp Dns1 = new DnsApp();
+        while (true ) {
+            System.out.println("*************le commandes ******");
+            System.out.println("1/rechercher une addres IP aprtir d'un nom de machine " +
+                    "Ecrire une nom de machine " +
+                    " (exp :machine.domaine.local) ");
+            System.out.println("2/rechercher un nom de machine aprtir d'une address Ip  " +
+                    "Ecrire une addres ip correcte (exp :192.168.0.1) ");
+            System.out.println("3/afficher liste machine par domaine donne   trie par leur nom :( ls)");
+            System.out.println("4/afficher liste machine par demaine donnne trie par leur addres ip  :( ls -a)");
+            Dns1.run();
+        }
+
+
+
+    }
+
 }
