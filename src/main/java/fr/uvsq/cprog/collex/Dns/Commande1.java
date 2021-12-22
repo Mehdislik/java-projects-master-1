0c19905d761d6bea527d5a442987cb11855f041b
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-22 04:56:52 +0100

    4.3/ ajout de la methode execute pour la classe Commande1 dans pakage DNS

--- src/main/java/fr/uvsq/cprog/collex/Dns/Commande1.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns/Commande1.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @author :debbah Mehdi sofiane 
+ * @author :debbah Mehdi sofiane
  * classe implemante l'interface Commande
  * cette classe vas excuter les commande saisie par les utlisateures
  */
@@ -27,7 +27,38 @@ import java.util.*;
          * @throws IOException
          */
         public String execute() throws IOException {
+            Dns dns = new Dns();
+            String result = "";
 
+            if (this.arg.equals("AddressIP")) {
+                NomMachine machine = dns.NomMachine_after_read(this.cmd);
+                result = dns.getItem(machine).getAdresseIP().toString();
+                return result;
+            }
+            if (this.arg.equals("NomMachine")) {
+                AdresseIP Ip=dns.IPaddres_after_read(this.cmd);
+                result = dns.getItem(Ip).getNomMachine().toString();
+                return result;
+
+            }
+            // liste machine meme domaine
+            if (this.arg.equals("ls")) {
+                ArrayList<String> listmachine = dns.getItems(cmd, 'm');
+                for(String dom :listmachine) {
+                    result = result + dom +"\n";
+                }
+                return result;
+            }
+            if (this.arg.equals("ls -a")) {
+                ArrayList<String> listmachine = dns.getItems(cmd, 'a');
+                for(String dom :listmachine) {
+                    result = result + dom +"\n";
+                }
+                return result;
+            } else {
+                System.out.println("Erreur :dans l'execution de la commmande ");
+                return null;
+            }
         }
     }
 
