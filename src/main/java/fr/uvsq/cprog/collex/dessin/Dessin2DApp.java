afc951916402974a284b2d296add8719e8aabbae
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-23 00:01:07 +0100

    package dessin :ajout de la classse main de dessin

---
+++ src/main/java/fr/uvsq/cprog/collex/dessin/Dessin2DApp.java
@@ -0,0 +1,14 @@
+package fr.uvsq.cprog.collex.dessin;
+
+public class Dessin2DApp {
+
+        public static void main(String[] args) {
+
+
+            Dessin2DTUI Dessin2DTUI = new Dessin2DTUI();
+            while (true){
+            ExAction Action = Dessin2DTUI.Input();
+            Action.execute();}
+        }
+    }
+
