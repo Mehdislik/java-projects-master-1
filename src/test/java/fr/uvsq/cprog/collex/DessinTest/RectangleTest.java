d8bb26fe3d16a84c756ce88dd4034562d4f46f65
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-23 01:24:33 +0100

    package dessin : ajout de la classe  test de rectangle avec le test du constructeur , deplacer

---
+++ src/test/java/fr/uvsq/cprog/collex/DessinTest/RectangleTest.java
@@ -0,0 +1,44 @@
+package fr.uvsq.cprog.collex.DessinTest;
+import fr.uvsq.cprog.collex.dessin.Point;
+import fr.uvsq.cprog.collex.dessin.Rectangle;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * simple test unitaire pour la  classe Rectangle
+ */
+public class RectangleTest {
+
+    @Test
+    public void test_constructeur (){
+        Point b_g = new Point(3,5) ;
+        Point  h_d = new Point(1, 1);
+        Rectangle r = new Rectangle(b_g,h_d,2,5);
+        assertEquals(new Point(3,5).getX() ,r.getBas_gauche().getX(),0);
+        assertEquals(new Point(3,5).getY() ,r.getBas_gauche().getY(),0);
+        assertEquals(new Point(1,1).getX() ,r.getHaut_droite().getX(),0);
+        assertEquals(new Point(1,1).getY() ,r.getHaut_droite().getY(),0);
+
+        assertEquals(2,r.getLength(),0);
+        assertEquals(5,r.getWidth(),0);
+
+
+    }
+
+    @Test
+    public  void test_deplacer(){
+        Point b_g = new Point(3,5) ;
+        Point  h_d = new Point(1, 1);
+        Rectangle r = new Rectangle(b_g,h_d,2,5);
+
+        r.deplacer(2,1);//deplacer l'axe x,y
+
+        assertEquals(5 ,r.getBas_gauche().getX(),0);
+        assertEquals(6 ,r.getBas_gauche().getY(),0);
+        assertEquals(3 ,r.getHaut_droite().getX(),0);
+        assertEquals(2,r.getHaut_droite().getY(),0);
+    }
+
+
+
+}
