ae896e2a3a24e7f28f2ddee94e80d4c7a3af9071
debbahMehdiSofiane <mehdi.debbah@ens.uvsq.fr>
2021-12-23 02:27:02 +0100

    package dessin : ajout test permietre et aire classe Triangle test

--- src/test/java/fr/uvsq/cprog/collex/DessinTest/TriangleTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DessinTest/TriangleTest.java
@@ -48,7 +48,25 @@ public class TriangleTest {
     }
 
 
+    @Test
+    public void test_perimetre (){
+        Point a = new Point(3,5) ;
+        Point  b = new Point(2, 1);
+        Point  c = new Point(2, 5);
+        Triangle t = new Triangle(a,b,c);
+        assertEquals(Math.sqrt(17)+5,t.Perimeter(),0);
 
+    }
+    @Test
+    public void test_aire (){
+        Point a = new Point(3,5) ;
+        Point  b = new Point(2, 1);
+        Point  c = new Point(2, 5);
+        Triangle t = new Triangle(a,b,c);
+        double smep= (Math.sqrt(17)+5)/2;
+        assertEquals(Math.sqrt(smep*(smep-t.coteA())*(smep-t.coteB())*(smep-t.coteC())),t.Aire(),0);
+
+    }
 
 
 }
